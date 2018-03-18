c23d29ab227c90096c67d71381d490461766ae08
Sebastian Schäfer <bschr9uwb@googlemail.com>
2018-03-18 19:20:45 +0100

    Adding poW to blockchain
    Signed-off-by: Sebastian Schäfer <bschr9uwb@googlemail.com>

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
 
+// Class for the Blocks the Chain will consist of later
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
@@ -7,22 +8,24 @@ class Block{
         this.timestamp = timestamp;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
+    // Calculating Hash
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + this.nonce + JSON.stringify(this.data)).toString();
     }
 
-        miningDifficulty(){
-        while(correctHash != true){
-            hash = this.calculateHash();
-            for(let i = 0; i<miningDifficultyNumber; i++){
-                if(hash[i] != 0){
-                    break;
-                }
-            }
-            correctHash = true;
-        }
+    // Including poW -> specific number of zeros at beginning of hash 
+    mineBlock(difficulty){
+       while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+           this.nonce += 1;
+           this.hash = this.calculateHash();
+       }
+
+       console.log("After " + this.nonce + " Successfull! -> Block Hash: " + this.hash);
+
+       return this.hash;
     }
 
 
@@ -31,17 +34,21 @@ class Block{
 class Blockchain {
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
+    // Creating the first (genesis) block
     createGenesisBlock(){
         return new Block("17/03/2018", "GenesisBlock", 0, "");
     }
 
+    // Querying for the latest block
     getLatestBlock(){
         return this.chain[this.chain.length - 1];
     }
 
-    verifyBlocks(){
+    // Verifying whole chain
+    verifyChain(){
         for(let i = 2; i < this.chain.length; i++){
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
@@ -58,44 +65,24 @@ class Blockchain {
             }   
     }
 
+    // Adding new Block to the chain
     addBLock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.hash = newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
-        // if (businessBlockchain.verifyBlocks() == false){
-        //     // throw "BLOCKCHAIN SECURITY DAMAGED";
-        //     console.log("BLockchain security damaged");
-        // }
-
-
-
-    }
-
-    isChainValid() {
-        for (let i = 1; i < this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
 
 let businessBlockchain = new Blockchain();
+console.log("First Block...");
 businessBlockchain.addBLock(new Block(1, '18/03/2018', { 'make' : 'Tesla', 'model' : 'Model S' }));
+console.log("Second Block...");
 businessBlockchain.addBLock(new Block(2, '19/04/2018', { 'make' : 'BMW' , 'model' : 'M2 Coupe'}));
+console.log("Third Block");
 businessBlockchain.addBLock(new Block(3, '19/04/2018', { 'make' : 'BMW' , 'model' : 'M3 Coupe'}));
-
+console.log("Fourth Block");
 businessBlockchain.addBLock(new Block(businessBlockchain.chain.length, '18/03/2018', { 'make' : 'Tesla', 'model' : 'Model X' }))
 
-console.log("Is chain valid ? " + businessBlockchain.isChainValid());
+console.log("Is chain valid ? " + businessBlockchain.verifyChain());
 
 console.log(JSON.stringify(businessBlockchain, null, 3));
