102d7de4d01f6b63e98ebbef1df819f8d1e21604
Snixells <bschr9uwb@googlemail.com>
2018-03-24 13:54:58 +0100

    Not yet implemented -> Posting and Getting Blockchain from server
    Signed-off-by: Snixells <bschr9uwb@googlemail.com>

--- network.js
+++ network.js
@@ -6,35 +6,49 @@ http.createServer((request, response) => {
     // Creates the body array
     let body = [];
 
-    // When an error occurs
-    request.on('error', (err) => {
-        // Print it to the console
-        console.error(err.stack);
-        // When data is beeing sent
-    }).on('data', (chunk) => {
-        // Push it to the end of body
-        body.push(chunk);
-        // And when the end is reached
-    }).on('end', () => {
-        // Save the array as string to body
-        body = Buffer.concat(body).toString;
+    // Implementing Blockchain functionality
+    if (request.method === 'POST' && request.url === '/blockchain') {
+        let body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString()
+            response.end(body);
+        });
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
 
-    // Listening for response errors
-    response.on('error', (err) => {
-        console.log(err);
-    });
+    // // When an error occurs
+    // request.on('error', (err) => {
+    //     // Print it to the console
+    //     console.error(err.stack);
+    //     // When data is beeing sent
+    // }).on('data', (chunk) => {
+    //     // Push it to the end of body
+    //     body.push(chunk);
+    //     // And when the end is reached
+    // }).on('end', () => {
+    //     // Save the array as string to body
+    //     body = Buffer.concat(body).toString;
 
-    // Setting response Headers
-    response.writeHead(200, {
-        'Content-Type': 'application/JSON',
-    });
+    //     // Listening for response errors
+    //     response.on('error', (err) => {
+    //         console.log(err);
+    //     });
 
-    const responseBody = { header, method, url, body}
+    //     // Setting response Headers
+    //     response.writeHead(200, {
+    //         'Content-Type': 'application/JSON',
+    //     });
 
-    // Setting response body
-    response.write(JSON.stringify(responseBody));
-    response.end();
+    //     const responseBody = { header, method, url, body }
+
+    //     // Setting response body
+    //     response.write(JSON.stringify(responseBody));
+    //     response.end();
 
 
-});
+    // });
 }).listen(8080);
